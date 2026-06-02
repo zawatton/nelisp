019560aa8802f46e31104fd1ed64ec86f093ec3a
zawatton <kurozawawo@gmail.com>
2026-06-03 04:51:46 +0900

    Support Windows IP header include sockopt

--- lisp/nelisp-stdlib-os.el
+++ lisp/nelisp-stdlib-os.el
@@ -74,6 +74,8 @@
 ;; int-valued socket option helpers.
 ;; Stage 101 maps `IPPROTO_IP' / `IP_PKTINFO' through the Windows int-valued
 ;; socket option helpers.
+;; Stage 102 maps `IPPROTO_IP' / `IP_HDRINCL' through the Windows int-valued
+;; socket option helpers.
 ;; Stage 19 maps `getppid' to the Tool Help process snapshot APIs.  Stage 20
 ;; adds a minimal Windows `fcntl' compatibility branch for `F_DUPFD' /
 ;; `F_GETFD' / `F_SETFD' / `F_GETFL' / `F_SETFL'.  Stage 21 rejects
@@ -260,6 +262,7 @@ Linux/BSD).  When nil, fall back to `nelisp-os--libc-call' libc bindings
 (defconst nelisp-os-WIN-SO-ERROR #x1007)
 (defconst nelisp-os-WIN-SO-TYPE #x1008)
 (defconst nelisp-os-WIN-IPPROTO-IP 0)
+(defconst nelisp-os-WIN-IP-HDRINCL 2)
 (defconst nelisp-os-WIN-IP-TTL 4)
 (defconst nelisp-os-WIN-IP-MULTICAST-IF 9)
 (defconst nelisp-os-WIN-IP-MULTICAST-TTL 10)
@@ -1943,6 +1946,7 @@ primitive; not supported in Phase 3."
 (defconst nelisp-os-SO-KEEPALIVE  9)
 (defconst nelisp-os-SO-OOBINLINE 10)
 (defconst nelisp-os-SO-ACCEPTCONN 30)
+(defconst nelisp-os-IP-HDRINCL 3)
 (defconst nelisp-os-IP-TTL        2)
 (defconst nelisp-os-IP-PKTINFO 8)
 (defconst nelisp-os-IP-MULTICAST-IF 32)
@@ -2429,6 +2433,9 @@ When GETTER-P is non-nil, include get-only options."
    ((and (= level nelisp-os-IPPROTO-TCP)
          (= optname nelisp-os-TCP-NODELAY))
     nelisp-os-WIN-TCP-NODELAY)
+   ((and (= level nelisp-os-IPPROTO-IP)
+         (= optname nelisp-os-IP-HDRINCL))
+    nelisp-os-WIN-IP-HDRINCL)
    ((and (= level nelisp-os-IPPROTO-IP)
          (= optname nelisp-os-IP-TTL))
     nelisp-os-WIN-IP-TTL)
