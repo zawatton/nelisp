2d0abc710a0d0adb87fa57aee07cbb76230dde90
zawatton <kurozawawo@gmail.com>
2026-06-03 04:31:48 +0900

    Support Windows IP multicast loop sockopt

--- lisp/nelisp-stdlib-os.el
+++ lisp/nelisp-stdlib-os.el
@@ -60,6 +60,8 @@
 ;; int-valued socket option helpers.
 ;; Stage 94 maps `IPPROTO_IP' / `IP_MULTICAST_TTL' through the Windows
 ;; int-valued socket option helpers.
+;; Stage 95 maps `IPPROTO_IP' / `IP_MULTICAST_LOOP' through the Windows
+;; int-valued socket option helpers.
 ;; Stage 19 maps `getppid' to the Tool Help process snapshot APIs.  Stage 20
 ;; adds a minimal Windows `fcntl' compatibility branch for `F_DUPFD' /
 ;; `F_GETFD' / `F_SETFD' / `F_GETFL' / `F_SETFL'.  Stage 21 rejects
@@ -248,6 +250,7 @@ Linux/BSD).  When nil, fall back to `nelisp-os--libc-call' libc bindings
 (defconst nelisp-os-WIN-IPPROTO-IP 0)
 (defconst nelisp-os-WIN-IP-TTL 4)
 (defconst nelisp-os-WIN-IP-MULTICAST-TTL 10)
+(defconst nelisp-os-WIN-IP-MULTICAST-LOOP 11)
 (defconst nelisp-os-WIN-IPPROTO-IPV6 41)
 (defconst nelisp-os-WIN-IPV6-UNICAST-HOPS 4)
 (defconst nelisp-os-WIN-IPV6-MULTICAST-HOPS 10)
@@ -1924,6 +1927,7 @@ primitive; not supported in Phase 3."
 (defconst nelisp-os-SO-ACCEPTCONN 30)
 (defconst nelisp-os-IP-TTL        2)
 (defconst nelisp-os-IP-MULTICAST-TTL 33)
+(defconst nelisp-os-IP-MULTICAST-LOOP 34)
 (defconst nelisp-os-IPV6-UNICAST-HOPS 16)
 (defconst nelisp-os-IPV6-MULTICAST-HOPS 18)
 (defconst nelisp-os-IPV6-V6ONLY 26)
@@ -2407,6 +2411,9 @@ When GETTER-P is non-nil, include get-only options."
    ((and (= level nelisp-os-IPPROTO-IP)
          (= optname nelisp-os-IP-MULTICAST-TTL))
     nelisp-os-WIN-IP-MULTICAST-TTL)
+   ((and (= level nelisp-os-IPPROTO-IP)
+         (= optname nelisp-os-IP-MULTICAST-LOOP))
+    nelisp-os-WIN-IP-MULTICAST-LOOP)
    ((and (= level nelisp-os-IPPROTO-IPV6)
          (= optname nelisp-os-IPV6-UNICAST-HOPS))
     nelisp-os-WIN-IPV6-UNICAST-HOPS)
